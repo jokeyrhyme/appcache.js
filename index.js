35ab8bcbffa744d7f66093798561aaa7caf1ab30
Ron Waldon <jokeyrhyme@gmail.com>
2015-04-10 15:35:26 +1000

    implement AppCache constructor and .parse()

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
 "use strict";
+
+function AppCache() {
+  this.cache = [];
+  this.network = [];
+  this.fallback = [];
+}
+
+AppCache.COMMENT = "#";
+
+AppCache.FIRST_LINE = "CACHE MANIFEST";
+
+AppCache.SECTIONS = ["CACHE", "FALLBACK", "NETWORK"];
+
+/**
+ * @param {String} fileContents the AppCache manifest file's contents
+ * @returns {AppCache} the parsed result
+ */
+AppCache.parse = function (fileContents) {
+  var appCache = new AppCache();
+  var entries;
+  var section;
+  if (!fileContents || typeof fileContents !== "string") {
+    return appCache;
+  }
+  entries = fileContents.split("\n");
+  if (entries[0] !== AppCache.FIRST_LINE) {
+    return appCache;
+  }
+  entries.forEach(function (line) {
+    var entry = line.trim();
+    var sectionName;
+    if (!entry || entry[0] === AppCache.COMMENT || entry === AppCache.FIRST_LINE) {
+      return;
+    }
+    sectionName = entry.replace(/:$/, "");
+    if (AppCache.SECTIONS.indexOf(sectionName) !== -1) {
+      section = appCache[sectionName.toLowerCase()];
+      return;
+    }
+    section.push(entry);
+  });
+  return appCache;
+};
+
+module.exports = AppCache;
--- tests/index.js
+++ tests/index.js
@@ -1,1 +1,78 @@
 "use strict";
+
+// Node.js built-ins
+
+var fs = require("fs");
+var path = require("path");
+
+// 3rd-party modules
+
+var test = require("tape");
+
+// our modules
+
+var AppCache = require("..");
+
+// this module
+
+require("tape-chai");
+
+test("AppCache", function (t) {
+  t.isFunction(AppCache);
+  t.end();
+});
+
+test("AppCache.parse", function (t) {
+  t.isFunction(AppCache.parse);
+  t.end();
+});
+
+test("new AppCache()", function (t) {
+  var appCache;
+  t.doesNotThrow(function () {
+    appCache = new AppCache();
+  });
+  t.isArray(appCache.cache);
+  t.isArray(appCache.fallback);
+  t.isArray(appCache.network);
+  t.end();
+});
+
+test("AppCache.parse('')", function (t) {
+  var appCache;
+  t.doesNotThrow(function () {
+    appCache = AppCache.parse("");
+  });
+  t.isArray(appCache.cache);
+  t.isArray(appCache.fallback);
+  t.isArray(appCache.network);
+  t.end();
+});
+
+test("AppCache.parse( >>html5rocks.appcache<< )", function (t) {
+  var expected = require("./fixtures/html5rocks.json");
+  fs.readFile(path.join(__dirname, "fixtures", "html5rocks.appcache"), {
+    encoding: "utf8"
+  }, function (err, contents) {
+    var appCache;
+    t.error(err);
+    t.isString(contents);
+    appCache = AppCache.parse(contents);
+    t.deepEqual(appCache, expected);
+    t.end();
+  });
+});
+
+test("AppCache.parse( >>splitsections.appcache<< )", function (t) {
+  var expected = require("./fixtures/splitsections.json");
+  fs.readFile(path.join(__dirname, "fixtures", "splitsections.appcache"), {
+    encoding: "utf8"
+  }, function (err, contents) {
+    var appCache;
+    t.error(err);
+    t.isString(contents);
+    appCache = AppCache.parse(contents);
+    t.deepEqual(appCache, expected);
+    t.end();
+  });
+});
